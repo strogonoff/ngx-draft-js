b965a516430a186e47a0bf5353079a3231f9dd4b
Anton Strogonoff <anton@strogonoff.name>
2018-01-25 20:39:28 +0900

    fix(rich-editor): export rich editor module

--- public-api.ts
+++ public-api.ts
@@ -1,3 +1,4 @@
 export * from './angular-draft-js/draft-js.module';
 export * from './angular-draft-js/draft-js.component';
+export * from './angular-draft-js/editors/rich.module';
 export * from './angular-draft-js/editors/rich.component';
