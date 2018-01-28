e0eb82eda2b22508a99352a7a0471b36eddbe363
Anton Strogonoff <anton@strogonoff.name>
2018-01-29 06:39:33 +0900

    packaging: roll back fesm2015 thing, cont’d

--- rollup-esm2015.conf.js
+++ rollup-esm2015.conf.js
@@ -1,5 +1,5 @@
 export default {
-  input: 'tmp/fesm2015/angular-draft-js.js',
+  input: 'tmp/esm2015/angular-draft-js.js',
   output: {
     file: 'dist/esm2015/angular-draft-js.js',
     format: 'es',
